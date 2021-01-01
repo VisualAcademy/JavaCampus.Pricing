59776681d0aae8a5bc5b5298f2a5e171b83b25ac
RedPlus <redplus@redplus.net>
2021-01-01 19:23:24 +0900

    PostMapping

--- java-campus/src/main/java/com/hawaso/javacampus/controllers/PricesController.java
+++ java-campus/src/main/java/com/hawaso/javacampus/controllers/PricesController.java
@@ -10,6 +10,10 @@ import com.hawaso.javacampus.services.PriceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -39,5 +43,12 @@ public class PricesController {
     @GetMapping("/prices/{id}")
     public Price getPriceById(@PathVariable int id) {
         return priceService.getPriceById(id); 
+    }
+
+    //@RequestMapping(method=RequestMethod.POST, value="/prices")
+    @PostMapping("/prices")
+    public void postPrice(@RequestBody Price model) {
+
+        priceService.addPrice(model);
     }
 }
--- java-campus/src/main/java/com/hawaso/javacampus/services/PriceService.java
+++ java-campus/src/main/java/com/hawaso/javacampus/services/PriceService.java
@@ -1,6 +1,7 @@
 package com.hawaso.javacampus.services;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,11 +11,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PriceService {
-    private List<Price> prices = Arrays.asList(
+    // private List<Price> prices = Arrays.asList(
+    //     new Price(1, "챔피언", "20시간", new BigDecimal(5_000.00)),
+    //     new Price(2, "스폰서", "100시간", new BigDecimal(30_000.00)),
+    //     new Price(3, "VIP", "모든", new BigDecimal(120_000.00))
+    // ); 
+    private List<Price> prices = new ArrayList<>(Arrays.asList(
         new Price(1, "챔피언", "20시간", new BigDecimal(5_000.00)),
         new Price(2, "스폰서", "100시간", new BigDecimal(30_000.00)),
         new Price(3, "VIP", "모든", new BigDecimal(120_000.00))
-    ); 
+    )); 
 
     public List<Price> getAllPrices() {
         return prices;
@@ -22,5 +28,10 @@ public class PriceService {
 
     public Price getPriceById(int id) {
         return prices.stream().filter(p -> p.getId() == id).findFirst().get();
+    }
+
+    public void addPrice(Price model) {
+        //prices.add(4, new Price(4, "test", "test", new BigDecimal(1000)));
+        prices.add(model); 
     }
 }
